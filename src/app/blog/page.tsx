1044e9351048a1c0e07f779399148e17f48e6a06
Evgeniya Osmakova <evgeniya.osmakova@gmail.com>
2025-03-06 20:02:42 +0100

    add metadata

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,8 +1,24 @@
 'use server'
 
+    import { Metadata } from 'next'
 import { BlogClient } from './BlogClient'
 import { BlogService } from '@/services/api'
 import { Post } from '@/types/post'
+
+export const metadata: Metadata = {
+    title: 'Blog Posts | Blog App',
+    description: 'Read our latest blog posts',
+    openGraph: {
+        title: 'Blog Posts | Blog App',
+        description: 'Read our latest blog posts',
+        type: 'website',
+    },
+    twitter: {
+        card: 'summary_large_image',
+        title: 'Blog Posts | Blog App',
+        description: 'Read our latest blog posts',
+    },
+}
 
 export default async function BlogPage() {
     const blogService = new BlogService();
